ac355c2f111333305ceb94df160e10097807570e
notequal21 <kalorigen@gmail.com>
2022-01-07 17:39:39 +0300

    01 task was solved

--- 01/index.js
+++ 01/index.js
@@ -1,1 +1,14 @@
 
+let anagrams = (word, words) => {
+  let anagram = []
+  let match = words.map(
+    (item) => {
+      if ([...word].sort().join('') == [...item].sort().join('')) {
+        anagram.push(item)
+      }
+    }
+  )
+  return anagram
+}
+
+console.log(anagrams('abba', ['aabb', 'abcd', 'bbaa', 'dada'])) // ['aabb', 'bbaa'] 
